108003c1aa4d4d1aeab3599fed33d7ed42460781
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-06-30 15:24:35 +0700

    feat: add google font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",'./src/**/*.{svelte,js,ts}'],
+  content: ["./index.html", './src/**/*.{svelte,js,ts}'],
   theme: {
     extend: {
+      fontFamily: {
+        'aref-ruqaa': ['"Aref Ruqaa Ink"', 'serif'],
+      },
       backgroundImage: {
         'pattern': "url('./assets/background/bg_main_pc.jpg')",
         'banner': "url('./assets/background/bg_ttl.png')",
         'banner-character': "url('./assets/background/t_ttl_character.png')",
-      }
+      },
     },
   },
   plugins: [],
 }
-
